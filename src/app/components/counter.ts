32ab113096c2ae8fa5be2d086719fb2fe1820930
crystas-sc <christus.t@outlook.com>
2020-04-16 16:15:55 +0530

    2 files modified

--- src/app/components/counter.ts
+++ src/app/components/counter.ts
@@ -7,16 +7,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 export class Counter {
 
    @Input() count: number = 0;
-  @Output() counterChange: EventEmitter<number> = new EventEmitter();
+  @Output() countChange: EventEmitter<number> = new EventEmitter();
   increment() {
     this.count = this.count + 1;
 
-    this.counterChange.emit(this.count);
+    this.countChange.emit(this.count);
   }
 
   decrement() {
     this.count = this.count - 1;
 
-    this.counterChange.emit(this.count);
+    this.countChange.emit(this.count);
   }
 }
